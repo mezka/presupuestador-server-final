14f313a2c07e0c3ed099073d75f11094d983d272
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-09-15 17:00:40 -0300

    Add setEagerLoadingForSomeFields hook

--- utils/hooks.js
+++ utils/hooks.js
@@ -10,6 +10,16 @@ module.exports = {
       };
       
       return context;
-    }
-  }
-}
+    };
+  },
+  setEagerLoadingForSomeFields: (modelnames) => {
+
+    return (context) => {
+      context.params.sequelize = {
+        include: typeof(modelnames) === 'string'? [{ model: context.app.service(modelnames).Model }] : modelnames.map((modelname) => ({model: context.app.service(modelname).Model})),
+        nest: true
+      };
+      return context;
+    };
+  },
+};
