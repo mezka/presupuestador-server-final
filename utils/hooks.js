4b6d18dd1ffeb1fdecf3818b613971c332ac77b6
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-09-17 00:05:20 -0300

    Fix setEagerLoadingForAllFields

--- utils/hooks.js
+++ utils/hooks.js
@@ -1,25 +1,17 @@
 module.exports = {
   setEagerLoadingForAllFields: () => {
     return (context) => {
-      
       context.params.sequelize = {
-        include: [{
-          all: true,
-        }],
-        nest: true
+        include: [
+          {
+            all: true
+          },
+        ],
+        nest: true,
+        raw: false
       };
-      
-      return context;
-    };
-  },
-  setEagerLoadingForSomeFields: (modelnames) => {
 
-    return (context) => {
-      context.params.sequelize = {
-        include: typeof(modelnames) === 'string'? [{ model: context.app.service(modelnames).Model }] : modelnames.map((modelname) => ({model: context.app.service(modelname).Model})),
-        nest: true
-      };
       return context;
     };
-  },
+  }
 };
